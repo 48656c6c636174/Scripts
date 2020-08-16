28aea0b1db2da32916fa947df681ace33787adee
48656c6c636174 <69371788+48656c6c636174@users.noreply.github.com>
2020-08-16 02:50:23 +0200

    Update HKalista.lua

--- HKalista.lua
+++ HKalista.lua
@@ -5,7 +5,7 @@ local Orb = require("lol/Modules/Common/Orb")
 local ts = require("lol/Modules/Common/simpleTS")
 
 local _Core = _G.CoreEx
-local ObjManager, EventManager, Input, Enums, Game  = _Core.ObjectManager, _Core.EventManager, _Core.Input, _Core.Enums, _Core.Game
+local ObjManager, EventManager, Input, Enums, Game, Renderer, Vector  = _Core.ObjectManager, _Core.EventManager, _Core.Input, _Core.Enums, _Core.Game, _Core.Renderer, _Core.Geometry.Vector
 local SpellSlots, SpellStates = Enums.SpellSlots, Enums.SpellStates
 local Player = ObjManager.Player
 
@@ -14,16 +14,17 @@ local _W = SpellSlots.W
 local _E = SpellSlots.E
 local _R = SpellSlots.R
 
+
 local function E_Logic(target, buffCount)
-	if buffCount > 0 and Player.BonusAD then
+	if buffCount > 0 and Player.TotalAD then
 		local Ebasedmg = {20, 30, 40, 50, 60}
 		local Estackdmg = {10, 14, 19, 25, 32}
 		local Estackadmg = {0.2, 0.2375, 0.275, 0.3125, 0.35}
 		local Ebaselvl = Ebasedmg[Player:GetSpell(_E).Level]
 		local Estacklvl = Estackdmg[Player:GetSpell(_E).Level]
 		local Estackalvl = Estackadmg[Player:GetSpell(_E).Level]
-		local Etotaldmg = Ebaselvl + Player.TotalAD * 0.6 + (Estacklvl + Player.TotalAD * Estackalvl) * buffCount
-		return Etotaldmg * (100.0 / (100 + target.Armor ) )
+		local Etotaldmg = Ebaselvl + Player.TotalAD * 0.6 + (Estacklvl + Player.TotalAD * Estackalvl) * (buffCount-1)
+		return Etotaldmg * (100.0 / (100 + target.Armor ) ) 
 	else
 		return 0
 	end
@@ -52,27 +53,57 @@ local function UseItems(target)
 		local _item = Player:GetSpell(i)
 		if _item ~= nil and _item then
 			local itemInfo = _item.Name
-
 			if itemInfo == "ItemSwordOfFeastAndFamine" or itemInfo == "BilgewaterCutlass" then
 				if Player:GetSpellState(i) == SpellStates.Ready then
-					Input.Cast(i, target)
+					if Player.Position:Distance(target.Position) <= 550 then
+						Input.Cast(i, target)
+					end
+					break
 				end
-				break
 			end
 		end
 	end
 end
 
+--[[local function buffdebug()
+		for i = 0, Player.BuffCount do
+			local buff = Player:GetBuff(i)
+			if buff then
+				Game.PrintChat(buff.Name)
+			end
+		end
+end]]
+
+--[[local function Qss()
+	for i=SpellSlots.Item1, SpellSlots.Item6 do
+		local _item = Player:GetSpell(i)
+		if _item ~= nil and _item then
+			local itemInfo = _item.Name	
+			if itemInfo == "QuicksilverSash" or itemInfo == "ItemMercurial" then
+				if Player:GetSpellState(i) == SpellStates.Ready then
+					for n = 0, Player.BuffCount do
+						local buff = Player:GetBuff(n)
+						if BuffInst.IsCC(buff) then
+							Input.Cast(i)
+						end
+					end
+				end
+				break
+			end
+		end
+	end
+end]]
+
 local function Combo(target)
 	if Player:GetSpellState(_E) == SpellStates.Ready then
-		local target = ts:GetTarget(1200,ts.Priority.LowestHealth)
+		local target = ts:GetTarget(1100,ts.Priority.LowestHealth)
 		local buffCountSpear = countEStacks(target)
 	end
 end
 
 local function AutoE()
 	local enemies = ObjManager.Get("enemy", "heroes")
-	local myPos, myRange = Player.Position, (Player.AttackRange + Player.BoundingRadius)	
+	local myPos, myRange = Player.Position, (Player.AttackRange + Player.BoundingRadius)
 	
 	if Player:GetSpellState(_E) ~= SpellStates.Ready then return end
 
@@ -82,8 +113,8 @@ local function AutoE()
 			local buffCountSpear = countEStacks(hero)
 			local dist = myPos:Distance(hero.Position)
 
-			if dist <= 1200 and buffCountSpear and E_Logic(hero,buffCountSpear) > (hero.Health) then				
-				Input.Cast(_E)      
+			if dist <= 1100 and buffCountSpear and E_Logic(hero,buffCountSpear) > (hero.Health) then				
+				Input.Cast(_E)       
 			end
 		end		
 	end	
@@ -94,18 +125,31 @@ local function OnTick()
 	local target = Orb.Mode.Combo and ts:GetTarget(Player.AttackRange + Player.BoundingRadius, ts.Priority.LowestHealth)
 	if target then 
 		Combo(target)
-		if Player.Position:Distance(target.Position) <= 550 then
-			UseItems(target)
-		end
+		UseItems(target)
 	end
 end
 
+local function OnDraw()
+	if Player:GetSpellState(_Q) == SpellStates.Ready then
+		local draw_Q = Renderer.DrawCircle3D(Player.Position, 1150, 30,1, 0xFF0000FF)
+	end
+	if Player:GetSpellState(_E) == SpellStates.Ready then
+		local draw_E = Renderer.DrawCircle3D(Player.Position, 1100, 30,1, 0xFFFF00FF)
+	end
+	local target = ts:GetTarget(1100,ts.Priority.LowestHealth)
+	if target then 
+		local E_Info = Renderer.DrawText(target.HealthBarScreenPos, Vector(100,100,0), 'E Damage: ' .. E_Logic(target,countEStacks(target)),  0xFFFFFFFF)
+		local Target_HP = Renderer.DrawText(target.HealthBarScreenPos-Vector(0,-10,0), Vector(110,100,0), 'Target HP: ' .. target.Health,  0xFFFFFFFF)
+	else 
+		local Target_Lost = Renderer.DrawText(Player.HealthBarScreenPos, Vector(100,100,0), 'Target Lost',  0xFFFFFFFF)
+	end
+end
 
 function OnLoad()
-    if not Player.CharName == "Kalista" then return false end
+	if not Player.CharName == "Kalista" then return false end
 	EventManager.RegisterCallback(Enums.Events.OnTick, OnTick)
+	EventManager.RegisterCallback(Enums.Events.OnDraw, OnDraw)
 	Orb.Load()
-	Orb.Setting.Drawing.BoundingRadius.EnemyMinion.Active = false
 	Game.PrintChat("HKalista Loaded!")
 	return true
 end
